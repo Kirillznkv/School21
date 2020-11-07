fe43505d66a44c0e9742c62fdc406b7e63156f84
Kenda Shanti <kshanti@si-e1.kzn.21-school.ru>
2020-11-07 20:13:07 +0300

    FIX MEMCHR

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,22 +6,22 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 19:11:15 by kshanti           #+#    #+#             */
-/*   Updated: 2020/11/01 15:06:43 by kshanti          ###   ########.fr       */
+/*   Updated: 2020/11/07 20:11:49 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memchr(const void *s, int c, size_t n)
+void			*ft_memchr(const void *s, int c, size_t n)
 {
-	int		i;
-	char	*str;
+	int				i;
+	unsigned char	*str;
 
 	i = 0;
-	str = (char*)s;
+	str = (unsigned char*)s;
 	while (n--)
 	{
-		if (str[i] == c)
+		if (str[i] == (unsigned char)c)
 			return (&str[i]);
 		i++;
 	}
