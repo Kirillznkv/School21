2c0f5e154abd258d94bc430f2db4576103e7d564
Kenda Shanti <kshanti@et-m3.kzn.21-school.ru>
2020-12-02 15:57:29 +0300

    print_str dont work, need debug

--- printf/srcs/ft_printf.c
+++ printf/srcs/ft_printf.c
@@ -6,38 +6,37 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:06:55 by kshanti           #+#    #+#             */
-/*   Updated: 2020/12/02 15:27:36 by kshanti          ###   ########.fr       */
+/*   Updated: 2020/12/02 15:56:29 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+void		print_str(char *s1, char *s2)
+{
+	while (s1 != s2)
+		ft_putchar_fd(*(s1++), 1);
+}
+
 int			ft_printf(const char *str, ...)
 {
 	va_list		va;
-	char		*s;
 	char		*p_newArg;
 	size_t		size;
 	t_arg		*elem;
 
-	size = ft_strlen(str);
-	if (!(s = ft_strdup(str)))
-		return (0);
 	va_start(va, str);
-	str = s;
 	elem = NULL;
-	while ((p_newArg = ft_strchr(s, '%')))
+	while ((p_newArg = ft_strchr(str, '%')))
 	{
-		*p_newArg = '\0';
-		ft_putstr_fd(s, 1);
+		print_str(str, p_newArg - 1);
 		if (!(elem = parser(p_newArg + 1, &va)))
 			return (0);
 		size += (processor(elem, &va) - elem->length);
-		s = p_newArg + elem->length;
+		str = p_newArg + elem->length;
 	}
-	ft_putstr_fd(s, 1);
+	ft_putstr_fd(str, 1);
 	va_end(va);
-	free((void*)str);
 	free(elem);
 	return (size);
 }
