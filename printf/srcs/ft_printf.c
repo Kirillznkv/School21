ac78fb64730e9bb2bb3a91cc7976aa9a73861c00
Kenda Shanti <kshanti@et-m3.kzn.21-school.ru>
2020-12-02 16:49:09 +0300

    fuck fuck fuck, iam checkout back, fuck

--- printf/srcs/ft_printf.c
+++ printf/srcs/ft_printf.c
@@ -6,40 +6,38 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:06:55 by kshanti           #+#    #+#             */
-/*   Updated: 2020/12/02 16:30:41 by kshanti          ###   ########.fr       */
+/*   Updated: 2020/12/02 15:27:36 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void		print_str(char *s1, char *s2)
-{
-	while (s1 != s2)
-		ft_putchar_fd(*(s1++), 1);
-}
-
 int			ft_printf(const char *str, ...)
 {
 	va_list		va;
+	char		*s;
 	char		*p_newArg;
 	size_t		size;
 	t_arg		*elem;
 
 	size = ft_strlen(str);
+	if (!(s = ft_strdup(str)))
+		return (0);
 	va_start(va, str);
+	str = s;
 	elem = NULL;
-	while ((p_newArg = ft_strchr(str, '%')))
+	while ((p_newArg = ft_strchr(s, '%')))
 	{
-		while (str != p_newArg)
-			ft_putchar_fd(*(str++), 1);
+		*p_newArg = '\0';
+		ft_putstr_fd(s, 1);
 		if (!(elem = parser(p_newArg + 1, &va)))
-			break ;
+			return (0);
 		size += (processor(elem, &va) - elem->length);
-		str = p_newArg + elem->length;
+		s = p_newArg + elem->length;
 	}
-	ft_putstr_fd((char*)str, 1);
+	ft_putstr_fd(s, 1);
 	va_end(va);
-	if (elem)
-		free(elem);
+	free((void*)str);
+	free(elem);
 	return (size);
 }
