ad15b58bbec2e224305861a2d35b8bb000936268
Kenda Shanti <kshanti@et-m3.kzn.21-school.ru>
2020-12-02 17:30:31 +0300

    its work, ft_printf fixed

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 14:37:19 by kshanti           #+#    #+#             */
-/*   Updated: 2020/12/02 15:13:58 by kshanti          ###   ########.fr       */
+/*   Updated: 2020/12/02 17:08:52 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ int			main()
 	int		c = 12345678;
 	char *d = "asdfg";
 
-    a = ft_printf("sadnkj%d\n", 5);
+    a = ft_printf("sadnkj%dksdhfhk%d\n", 5, 5);
     b = printf("%5p\n", NULL);
 
 	printf("ft_p = %d\np    = %d\n\n", a, b);
+	while (1)
+		;
 	return (0);
 }
--- printf/srcs/ft_printf.c
+++ printf/srcs/ft_printf.c
@@ -6,11 +6,17 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:06:55 by kshanti           #+#    #+#             */
-/*   Updated: 2020/12/02 15:27:36 by kshanti          ###   ########.fr       */
+/*   Updated: 2020/12/02 17:28:06 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+void		print_str(char *s1, char *s)
+{
+		*s1 = '\0';
+		ft_putstr_fd(s, 1);
+}
 
 int			ft_printf(const char *str, ...)
 {
@@ -25,19 +31,18 @@ int			ft_printf(const char *str, ...)
 		return (0);
 	va_start(va, str);
 	str = s;
-	elem = NULL;
 	while ((p_newArg = ft_strchr(s, '%')))
 	{
-		*p_newArg = '\0';
-		ft_putstr_fd(s, 1);
+		elem = NULL;
+		print_str(p_newArg, s);
 		if (!(elem = parser(p_newArg + 1, &va)))
-			return (0);
+			break ;
 		size += (processor(elem, &va) - elem->length);
 		s = p_newArg + elem->length;
+		free(elem);
 	}
 	ft_putstr_fd(s, 1);
 	va_end(va);
 	free((void*)str);
-	free(elem);
 	return (size);
 }
